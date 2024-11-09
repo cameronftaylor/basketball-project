d17fa1fd0aeaf0d888e2e8d9513e9e453fba4c05
cameronftaylor <60576735+cameronftaylor@users.noreply.github.com>
2024-11-09 10:31:38 +0100

    inital commit

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
 let num1 = 0
 let num2 = 0
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+let num1El = document.getElementById("num1-el")
+let num2El = document.getElementById("num2-el")
+
+// Initialize the display
+num1El.textContent = num1
+num2El.textContent = num2
+
+function addHomeOne() {
+    num1 += 1
+    num1El.textContent = num1
+}
+
+function addHomeTwo() {
+    num1 += 2
+    num1El.textContent = num1
+}
+
+function addHomeThree() {
+    num1 += 3
+    num1El.textContent = num1
+}
+
+function addGuestOne() {
+    num2 += 1
+    num2El.textContent = num2
+}
+
+function addGuestTwo() {
+    num2 += 2
+    num2El.textContent = num2
+}
+
+function addGuestThree() {
+    num2 += 3
+    num2El.textContent = num2
+}
+
+function clearHomeCount() {
+    num1El.textContent = 0
+    num1 = 0
+}
+
+function clearGuestCount() {
+    num2El.textContent = 0
+    num2 = 0
+}
